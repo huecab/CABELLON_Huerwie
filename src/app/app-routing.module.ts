a6254f63c04bc4bbacbf7e15118094b28b7b7b84
huecab <huerwiecabellon@gmail.com>
2024-04-03 09:42:28 +0800

    Activity 7 Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardPageModule),
-    // canActivate: [AuthenticationService],
+    canActivate: [AuthenticationService],
   },
   {
     path: 'calculator',
